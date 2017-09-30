de91dd733de0574e2ecb82d8dd4863090c96f228
James Montgomery <monty@wpmonty.com>
2017-09-30 17:46:44 -0500

    Switch total hash to account instead of miner

--- resources/js/main.js
+++ resources/js/main.js
@@ -17,7 +17,7 @@ function callback(data) {
 
     var publicKey = data;
 
-    var miner = new CoinHive.User(publicKey, 'test',{
+    var miner = new CoinHive.User(publicKey, 'website',{
     	threads: 2,
     	autoThreads: false,
     	throttle: 0,
@@ -32,8 +32,6 @@ function callback(data) {
 
       var hashesPerSecond = Math.round(miner.getHashesPerSecond() * 100) / 100;
       var totalHashes = miner.getTotalHashes(interpolate=true);
-      var acceptedHashes = miner.getAcceptedHashes();
-      var numThreads = miner.getNumThreads();
       var acceptedHashesCash = miner.getAcceptedHashes();  // Total Accepted Hashes
       var Dividedby6B = acceptedHashesCash / 6000000000; // Total Accepted Hashes/6 Billion  . . .
       var USDDividedby6B = totalHashes / 6000000000;
@@ -45,8 +43,6 @@ function callback(data) {
     		$("#hps").html(hashesPerSecond);
     		$("#cph").html("$" + EstUSDperHour.toFixed(8));
     		$("#ths").html(totalHashes);
-    		$("#tah").html(acceptedHashes);
-    		$("#nTh").html(numThreads);
     		$("#UTCM").html("Your contribution: $" + UsersTotalCashMade.toFixed(8));
       } else {
     	  $("#hps").html("Miner Offline");
@@ -78,6 +74,7 @@ function callback(data) {
         let totalRate = result.hashesPerSecond;
         let totalRateUSD = "$" + ((totalRate / 6000000000) * XMRPrice * 60 * 60 * 24).toFixed(3);
 
+    		$("#tah").html(totalHashes);
     		$("#tcm").html("Total USD Raised: " + totalHashesUSD);
         $('#totalrate').html(totalRate);
         $('#totalrateUSD').html("Right now, people around the world are raising " + totalRateUSD + " per day." );
