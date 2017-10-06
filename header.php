770fe926bb0d4156815ecfb6d183970cad7df981
Ryan Visek <ryanvisek@gmail.com>
2017-10-05 22:33:56 -0700

    added facebook app

--- header.php
+++ header.php
@@ -19,6 +19,24 @@
     js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1752758421415804";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));</script>
+  <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2012451592332992',
+      xfbml      : true,
+      version    : 'v2.10'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
   <div id="top-nav">
     <div class="container">
