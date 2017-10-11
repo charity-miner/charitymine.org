ec2f60c36761fa4a2855ee4bee87d5763ece3ea9
Ryan Visek <ryanvisek@gmail.com>
2017-10-11 12:04:35 -0700

    Fixing facebook scripts

--- header.php
+++ header.php
@@ -12,14 +12,7 @@
 
   <!-- Facebook's SDK - for the share button -->
   <div id="fb-root"></div>
-  <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=1752758421415804";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
-  <script>
+<script>
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '2012451592332992',
@@ -36,6 +29,7 @@
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+
 </script>
 
   <div id="top-nav">
